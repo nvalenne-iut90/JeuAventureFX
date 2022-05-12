eec365c13dc531acf9acf97d3f536f80c73272c8
VALENNE Nathan <nathan.valenne@edu.univ-fcomte.fr>
2022-05-12 23:09:50 +0200

    View + Model

--- src/gameFX/Jeu.java
+++ src/gameFX/Jeu.java
@@ -1,7 +1,10 @@
 package gameFX;
 
 
+import gameFX.control.events.ControlButton;
+import gameFX.model.Model;
 import gameFX.model.personnage.player.Joueur;
+import gameFX.view.BasicView;
 import gameFX.view.stage.Hub;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -13,8 +16,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -24,17 +27,16 @@ import java.util.concurrent.TimeUnit;
 public class Jeu extends Application {
     public static Scanner scan = new Scanner(System.in);
     public static Joueur player;
-    public static Scene firstScene;
-    public static final int X_WINDOW = 600;
-    public static final int Y_WINDOW = 800;
+    public static Scene startScene;
+    public static final int X_WINDOW = 500;
+    public static final int Y_WINDOW = 600;
 
+    public TextField playerName;
+    public Button play;
+    public ToggleGroup tgSex;
 
-
-    TextField playerName;
-    Button play;
     RadioButton male;
     RadioButton female;
-    ToggleGroup tgSex;
     VBox vbPlayer;
     HBox hbSex;
     HBox hbLogo;
@@ -46,16 +48,17 @@ public class Jeu extends Application {
         System.exit(0);
     }
 
-    private Scene createScene(){
+    private Scene createStartScene(){
+
         final URL imageURL = getClass().getResource("model/assets/logoJeu.png");
         final ImageView imageView = new ImageView(new Image(imageURL.toExternalForm()));
-        imageView.setScaleX(0.7);
-        imageView.setScaleY(0.8);
+        imageView.setFitWidth(300);
+        imageView.setFitHeight(200);
 
         play = new Button("PLAY");
         play.setStyle("-fx-background-color: #00adff;");
-        male = new RadioButton("masculin");
-        female = new RadioButton("féminin");
+        male = new RadioButton("Masculin");
+        female = new RadioButton("Féminin");
         tgSex = new ToggleGroup();
         male.setToggleGroup(tgSex);
         female.setToggleGroup(tgSex);
@@ -63,7 +66,7 @@ public class Jeu extends Application {
         playerName = new TextField();
         playerName.setPromptText("Enter your name");
         playerName.setMaxWidth(250);
-        playerName.setStyle("-fx-background-color: rgba(250, 184, 114, 0.5);");
+        // playerName.setStyle("-fx-background-color: rgba(250, 184, 114, 0.5);");
 
         hbSex = new HBox();
         hbLogo = new HBox();
@@ -75,22 +78,19 @@ public class Jeu extends Application {
         hbSex.setAlignment(Pos.CENTER);
         vbPlayer.getChildren().addAll(hbLogo, playerName, hbSex, play);
         vbPlayer.setAlignment(Pos.CENTER);
+        vbPlayer.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
         VBox.setMargin(playerName, new Insets(15));
         VBox.setMargin(hbSex, new Insets(15));
 
-        firstScene = new Scene(vbPlayer,X_WINDOW,Y_WINDOW);
+        startScene = new Scene(vbPlayer,X_WINDOW,Y_WINDOW);
         // firstScene.getStylesheets().add("styles/stylePlayer.css");
-        return firstScene;
+        return startScene;
 
     }
 
     private void addListeners(){
-        play.setOnAction(actionEvent -> {
-            String sexe = ((RadioButton)male.getToggleGroup().getSelectedToggle()).getText();
-            player = new Joueur(playerName.getText(), sexe);// /!\ Création du joueur /!\
-
-            Hub.lancer(player);
-        });
+        ControlButton controlButton = new ControlButton(this);
+        play.setOnAction(controlButton);
     }
 
 
@@ -100,7 +100,7 @@ public class Jeu extends Application {
 
     @Override
     public void start(Stage stage) {
-        stage.setScene(createScene());
+        stage.setScene(createStartScene());
         addListeners();
         stage.setTitle("Jeu Aventure");
         stage.setResizable(false);

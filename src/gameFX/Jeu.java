e0c2e2ed5fa2c7fdea20e2aa14294b7e8d73dcd8
VALENNE Nathan <nathan.valenne@edu.univ-fcomte.fr>
2022-05-16 20:31:24 +0200

    Corrections Shop

--- src/gameFX/Jeu.java
+++ src/gameFX/Jeu.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -38,6 +39,7 @@ public class Jeu extends Application {
 
 
         stage.setScene(scene);
+        stage.getIcons().add(new Image("gameFX/model/assets/icon.png"));
         stage.setTitle("Jeu Aventure");
         stage.setResizable(false);
         stage.show();
--- src/gameFX/control/events/ShopController.java
+++ src/gameFX/control/events/ShopController.java
@@ -2,21 +2,17 @@ package gameFX.control.events;
 
 import gameFX.Jeu;
 import gameFX.model.item.Item;
+import gameFX.model.personnage.player.Inventory;
 import gameFX.model.personnage.pnj.Marchand;
 import gameFX.view.stage.Shop;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.fxml.*;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
-import java.sql.BatchUpdateException;
 import java.util.ResourceBundle;
 
 public class ShopController implements Initializable {
@@ -34,8 +30,20 @@ public class ShopController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         shop = new Shop();
         articlesVente = shop.getArticlesVente();
-
         setLabel();
+        showArticles();
+
+    }
+
+    private void setLabel(){
+        if ((Jeu.player.getSexe()).equals("Masculin")) {
+            welcome.setText("Bienvenue étranger ! Approchez !! Approchez !!");
+        } else if ((Jeu.player.getSexe()).equals("Féminin")) {
+            welcome.setText("Bienvenue étrangère ! Approchez !! Approchez !!");
+        }
+    }
+
+    private void showArticles(){
         Marchand marchand = new Marchand("Pierre", "Marchand");
         namePNJ.setText("Marchand " + marchand.getNom());
         namePNJ.setTextFill(Color.GREEN);
@@ -48,21 +56,26 @@ public class ShopController implements Initializable {
             buy.setStyle("-fx-background-color: green; -fx-text-fill: white;");
 
             hbox.getChildren().addAll(label, buy);
-            HBox.setMargin(hbox, new Insets(30,0,50,0));
-
+            hbox.setPadding(new Insets(10));
 
             articles.getChildren().add(hbox);
         }
-
     }
 
-    private void setLabel(){
-        if ((Jeu.player.getSexe()).equals("Masculin")) {
-            welcome.setText("Bienvenue cher étranger ! Approchez !! Approchez !!");
-        } else if ((Jeu.player.getSexe()).equals("Féminin")) {
-            welcome.setText("Bienvenue chère étrangère ! Approchez !! Approchez !!");
+    private void showInventory(){
+        Inventory inventory = Jeu.player.getInventory();
+        for (Item item : inventory.getInventory()){
+            HBox hbox = new HBox();
+            Label label = new Label(item.getNameItem() + " : " + item.getPrice() + " pièces d'or");
+            label.setStyle("-fx-font-size: 20px;-fx-text-alignment: center;");
+            HBox.setMargin(label, new Insets(0,10,0,0));
+            Button buy = new Button("VENDRE");
+            buy.setStyle("-fx-background-color: orange; -fx-text-fill: white;");
+
+            hbox.getChildren().addAll(label, buy);
+            hbox.setPadding(new Insets(10));
+
+            articles.getChildren().add(hbox);
         }
-    }
-    private void showArticles(){
     }
 }
